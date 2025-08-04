0a951b997bc0c1d2eb0ae18d5538396e5bef3c5e
Sofia Sosa <sosasofiabeatriz@gmail.com>
2025-08-04 20:50:44 +0000

    Add SQL query to list employees with their managers' names

--- Scripts/00-03.sql
+++ Scripts/00-03.sql
@@ -11,3 +11,16 @@ LIMIT 5;
 SELECT sql 
 FROM sqlite_schema 
 WHERE name = 'employee';
+/*
+List of employees, including first and last name, and the 
+first and last names of their inmediate managers
+*/
+
+SELECT 
+  emp.firstName,
+  emp.title,
+  mng.firstName AS ManagerFirstName,
+  mng.lastName AS ManagerLastName
+FROM employee emp
+INNER JOIN employee mng
+  ON emp.managerId = mng.employeeId;
